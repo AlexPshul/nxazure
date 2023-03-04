98e843756e66cd6ac1a1d42989c9508e87910d58
Alex Pshul <alex@pshul.com>
2023-03-04 13:36:33 -0500

    Fixed jest warnings

--- e2e/func-e2e/jest.config.ts
+++ e2e/func-e2e/jest.config.ts
@@ -2,13 +2,8 @@
 export default {
   displayName: 'func-e2e',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/e2e/func-e2e',
