043f1263ad0e4a4ed7c92f51ecd72ca1c4f7218c
Fran Aguilera <fraguile@microsoft.com>
2023-04-26 18:58:21 -0400

    Added other start executor schema options for Azure Functions.

--- packages/func/src/executors/start/executor.ts
+++ packages/func/src/executors/start/executor.ts
@@ -9,7 +9,48 @@ const executor: Executor<StartExecutorSchema> = async (options, context) => {
   if (success) {
     const cwd = context.workspace?.projects[context.projectName].root;
 
-    execSync(`func start --port ${options.port}`, {
+    const {
+      port,
+      cors,
+      corsCredentials,
+      timeout,
+      useHttps,
+      cert,
+      password,
+      languageWorker,
+      noBuild,
+      enableAuth,
+      functions,
+      verbose,
+      dotnetIsolatedDebug,
+      enableJsonOutput,
+      jsonOutputFile,
+    } = options;
+
+    const args = [
+      { flag: '--port', value: port },
+      { flag: '--cors', value: cors },
+      { flag: '--cors-credentials', value: corsCredentials },
+      { flag: '--timeout', value: timeout },
+      { flag: '--useHttps', value: useHttps },
+      { flag: '--cert', value: cert, condition: useHttps },
+      { flag: '--password', value: password, condition: cert },
+      { flag: '--language-worker', value: languageWorker },
+      { flag: '--no-build', value: noBuild },
+      { flag: '--enableAuth', value: enableAuth },
+      { flag: '--functions', value: functions },
+      { flag: '--verbose', value: verbose },
+      { flag: '--dotnet-isolated-debug', dotnetIsolatedDebug },
+      { flag: '--enable-json-output', value: enableJsonOutput },
+      { flag: '--json-output-file', value: jsonOutputFile, condition: enableJsonOutput },
+    ];
+
+    const command = args
+      .filter(arg => arg.value && (!arg.condition || arg.condition))
+      .map(arg => `${arg.flag} ${arg.value}`)
+      .join(' ');
+
+    execSync(`func start ${command}`, {
       cwd,
       stdio: 'inherit',
     });
--- packages/func/src/executors/start/schema.d.ts
+++ packages/func/src/executors/start/schema.d.ts
@@ -1,3 +1,17 @@
 export interface StartExecutorSchema {
   port: number;
+  cors: string;
+  corsCredentials: boolean;
+  timeout: number;
+  useHttps: boolean;
+  cert: string;
+  password: string;
+  languageWorker: string;
+  noBuild: boolean;
+  enableAuth: boolean;
+  functions: string;
+  verbose: boolean;
+  dotnetIsolatedDebug: boolean;
+  enableJsonOutput: boolean;
+  jsonOutputFile: string;
 } // eslint-disable-line
