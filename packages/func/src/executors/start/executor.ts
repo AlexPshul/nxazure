f9c042e3374d34dc9bc0b1de7bff3e36acd4a7c6
Fran Aguilera <fraguile@microsoft.com>
2023-05-02 14:43:11 -0400

    Use additionalFlags instead.

--- packages/func/src/executors/start/executor.ts
+++ packages/func/src/executors/start/executor.ts
@@ -1,7 +1,7 @@
-import { StartExecutorSchema } from './schema';
 import { Executor } from '@nrwl/devkit';
-import { build } from '../common/utils';
 import { execSync } from 'child_process';
+import { build } from '../common/utils';
+import { StartExecutorSchema } from './schema';
 
 const executor: Executor<StartExecutorSchema> = async (options, context) => {
   const success = build(context);
@@ -9,48 +9,7 @@ const executor: Executor<StartExecutorSchema> = async (options, context) => {
   if (success) {
     const cwd = context.workspace?.projects[context.projectName].root;
 
-    const {
-      port,
-      cors,
-      corsCredentials,
-      timeout,
-      useHttps,
-      cert,
-      password,
-      languageWorker,
-      noBuild,
-      enableAuth,
-      functions,
-      verbose,
-      dotnetIsolatedDebug,
-      enableJsonOutput,
-      jsonOutputFile,
-    } = options;
-
-    const args = [
-      { flag: '--port', value: port },
-      { flag: '--cors', value: cors },
-      { flag: '--cors-credentials', value: corsCredentials },
-      { flag: '--timeout', value: timeout },
-      { flag: '--useHttps', value: useHttps },
-      { flag: '--cert', value: cert, condition: useHttps },
-      { flag: '--password', value: password, condition: cert },
-      { flag: '--language-worker', value: languageWorker },
-      { flag: '--no-build', value: noBuild },
-      { flag: '--enableAuth', value: enableAuth },
-      { flag: '--functions', value: functions },
-      { flag: '--verbose', value: verbose },
-      { flag: '--dotnet-isolated-debug', dotnetIsolatedDebug },
-      { flag: '--enable-json-output', value: enableJsonOutput },
-      { flag: '--json-output-file', value: jsonOutputFile, condition: enableJsonOutput },
-    ];
-
-    const command = args
-      .filter(arg => arg.value && (!arg.condition || arg.condition))
-      .map(arg => `${arg.flag} ${arg.value}`)
-      .join(' ');
-
-    execSync(`func start ${command}`, {
+    execSync(`func start ${options.additionalFlags}`, {
       cwd,
       stdio: 'inherit',
     });
--- packages/func/src/executors/start/schema.d.ts
+++ packages/func/src/executors/start/schema.d.ts
@@ -1,17 +1,3 @@
 export interface StartExecutorSchema {
-  port: number;
-  cors: string;
-  corsCredentials: boolean;
-  timeout: number;
-  useHttps: boolean;
-  cert: string;
-  password: string;
-  languageWorker: string;
-  noBuild: boolean;
-  enableAuth: boolean;
-  functions: string;
-  verbose: boolean;
-  dotnetIsolatedDebug: boolean;
-  enableJsonOutput: boolean;
-  jsonOutputFile: string;
+  additionalFlags: string;
 } // eslint-disable-line
