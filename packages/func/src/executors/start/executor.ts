bc773b3ba7b172c7b0a13b6ae3726f17757082b4
Francisco Aguilera <fraguile@microsoft.com>
2023-05-06 17:13:23 -0400

    Added other start executor schema options for Azure Functions. (#13)
    * Added other start executor schema options for Azure Functions.
    
    * Use additionalFlags instead.
    
    * Bring back port option for init generator use.

--- packages/func/src/executors/start/executor.ts
+++ packages/func/src/executors/start/executor.ts
@@ -1,15 +1,22 @@
-import { StartExecutorSchema } from './schema';
 import { Executor } from '@nrwl/devkit';
-import { build } from '../common/utils';
 import { execSync } from 'child_process';
+import { build } from '../common/utils';
+import { StartExecutorSchema } from './schema';
 
 const executor: Executor<StartExecutorSchema> = async (options, context) => {
   const success = build(context);
 
   if (success) {
-    const cwd = context.workspace?.projects[context.projectName].root;
+    const { workspace, projectName, isVerbose, target } = context;
+    const cwd = workspace?.projects[projectName].root;
 
-    execSync(`func start --port ${options.port}`, {
+    const { port, additionalFlags } = options;
+    const command = `func start --port ${port} ${additionalFlags}`;
+    if (isVerbose) {
+      console.log(`Running ${target.executor} command: ${command}.`);
+    }
+
+    execSync(command, {
       cwd,
       stdio: 'inherit',
     });
