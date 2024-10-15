9fe70818535f835fc7ded8203ad6a3e8a85c38c9
Alex Pshul <alex@pshul.com>
2024-10-14 21:44:52 -0400

    Allow overriding env vars from .env files by setting local.settings.json file

--- packages/func/src/executors/start/executor.ts
+++ packages/func/src/executors/start/executor.ts
@@ -1,10 +1,24 @@
-import { Executor } from '@nx/devkit';
+import { Executor, readJsonFile } from '@nx/devkit';
 import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { fileExists } from 'nx/src/utils/fileutils';
 import treeKill from 'tree-kill';
 import { color } from '../../common';
 import { build, watch } from '../common';
 import { FuncLogger } from './func-logger';
 import { StartExecutorSchema } from './schema';
+
+const loadProcessEnvWithoutOverrides = (projectCwd: string) => {
+  const localProcessEnvCopy = { ...process.env };
+  const localSettingsJsonPath = `${projectCwd}/local.settings.json`;
+  if (!fileExists(localSettingsJsonPath)) return localProcessEnvCopy;
+
+  const localSettingsConfig = readJsonFile<{ Values: Record<string, string> }>(localSettingsJsonPath).Values;
+  if (!localSettingsConfig) return localProcessEnvCopy;
+
+  Object.keys(localSettingsConfig).forEach(key => delete localProcessEnvCopy[key]);
+
+  return localProcessEnvCopy;
+};
 
 const executor: Executor<StartExecutorSchema> = async (options, context) => {
   const { port, disableWatch, additionalFlags } = options;
@@ -20,7 +34,8 @@ const executor: Executor<StartExecutorSchema> = async (options, context) => {
     if (isVerbose) console.log(`Running ${target.executor} command: func ${params.join(' ')}.`);
 
     const cwd = workspace?.projects[projectName].root;
-    spawned = spawn('func', params, { cwd, detached: false, shell: true });
+    const noOverridesEnvVars = loadProcessEnvWithoutOverrides(cwd);
+    spawned = spawn('func', params, { cwd, detached: false, shell: true, env: noOverridesEnvVars });
 
     spawned.stdout.on('data', data => logger.logData(data?.toString()));
     spawned.stderr.on('data', data => logger.logError(data?.toString()));
